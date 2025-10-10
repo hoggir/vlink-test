21c3e0e12dc251415f2ef66b3fad5f0d036aa56e
fadelgiras <fadel.paypal13@gmail.com>
2025-10-11 01:09:34 +0700

    fix ge

--- fe-book/next.config.mjs
+++ fe-book/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Base path untuk production di nginx /book
+  basePath: process.env.NODE_ENV === 'production' ? '/book' : '',
+
+  // Asset prefix untuk static files
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/book' : '',
+};
 
 export default nextConfig;
